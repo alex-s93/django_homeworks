2ae17b3f1c81e966b09008dd094504067a39b5ca
alex-s93 <solodkov.alex@gmail.com>
2024-05-27 17:21:46 +0200

    django hw 7: add fixes and improvements

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
