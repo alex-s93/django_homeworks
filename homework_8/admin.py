bef9e7f197c1d00088965c46bb4954ed14899a59
alex-s93 <solodkov.alex@gmail.com>
2024-06-03 16:00:52 +0200

    django: add homework 9

--- homework_8/admin.py
+++ homework_8/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 
 from homework_8.models import Task, Subtask, Category
 
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(Subtask)
-admin.site.register(Category)
+
+class SubtaskInline(admin.StackedInline):
+    model = Subtask
+    extra = 1
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [SubtaskInline]
+    list_display = ('title', 'get_categories', 'status', 'deadline', 'created_at')
+
+    def get_categories(self, task):
+        return ", ".join(category.name for category in task.categories.all())
+
+    get_categories.short_description = 'Categories'
+
+
+@admin.register(Subtask)
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline', 'created_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
---
+++ homework_8/models/statuses.py
@@ -0,0 +1,7 @@
+STATUSES = [
+    ("New", "New"),
+    ("In progress", "In progress"),
+    ("Pending", "Pending"),
+    ("Blocked", "Blocked"),
+    ("Done", "Done")
+]
